42eff8aabd0fdb639922a42f515043e3cbc642ce
Eric0910 <erickithinji0910@gmail.com>
2024-02-20 11:09:57 +0300

    added an alert window in a button

---
+++ index.js
@@ -0,0 +1,8 @@
+ const firstinvestbtn =document.getElementById("firstInvestbtn");
+ function display(){
+    var newWindow = window.open("", "_blank", "width=200,height=100");
+    newWindow.document.write("<h1>Hello</h1>");
+   
+    
+    
+ }
